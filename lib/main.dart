249af492a505952e3dab918729cbb243fc3113e6
ingMauricioFlores <mauricio.flores@impuls.com.mx>
2025-10-17 18:38:06 -0600

    ramaPruebas

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter MASTER',
+      title: 'Flutter PRUEBAS',
       theme: ThemeData(
         // This is the theme of your application.
         //
